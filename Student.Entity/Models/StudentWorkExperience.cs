dab0e7fede704b3960a90f5fce62f4be33497055
Neha Gupta <neha.gupta@exxatsystems.com>
2019-02-03 01:30:17 +0530

    StudentWorkExperience

---
+++ Student.Entity/Models/StudentWorkExperience.cs
@@ -0,0 +1,26 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student.Entity.Models
+{
+    [FirestoreData]
+    class StudentWorkExperience
+
+    { 
+        [FirestoreProperty]
+        public Guid ProfileKey { get; set; }
+        [FirestoreProperty]
+        public string OrganisationName { get; set; }
+        [FirestoreProperty]
+        public string Role { get; set; }
+        [FirestoreProperty]
+        public string Responsibility { get; set; }
+        [FirestoreProperty]
+        public DateTime FromDate { get; set; }
+        [FirestoreProperty]
+        public DateTime ToDate { get; set; }
+
+    }
+}
